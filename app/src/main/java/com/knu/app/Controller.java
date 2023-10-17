ed263ead62e7079268ac99577933ce56a3af1973
Pavlo Kilko <pavel.kilko@gmail.com>
2023-10-17 08:01:40 +0300

    feat: add Spring App

---
+++ app/src/main/java/com/knu/app/Controller.java
@@ -0,0 +1,29 @@
+package com.knu.app;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/everyone")
+    public String helloEveryone() {
+        return "Hello Everyone from Spring Boot & Keycloak";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('client_user')")
+    public String helloUser() {
+        return "Hello User from Spring Boot & Keycloak";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('client_admin')")
+    public String helloAdmin() {
+        return "Hello Admin from Spring Boot & Keycloak";
+    }
+}
