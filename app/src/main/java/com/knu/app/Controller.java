fd3a87155b7ea63b2df2166aaadf25aa5e88a681
Pavlo Kilko <pavel.kilko@gmail.com>
2023-11-08 21:07:45 +0200

    feat: configure Keycloak

--- app/src/main/java/com/knu/app/Controller.java
+++ app/src/main/java/com/knu/app/Controller.java
@@ -7,23 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/demo")
 public class Controller {
-
-    @GetMapping("/everyone")
-    public String helloEveryone() {
-        return "Hello Everyone from Spring Boot & Keycloak";
-    }
 
     @GetMapping("/user")
     @PreAuthorize("hasRole('client_user')")
     public String helloUser() {
-        return "Hello User from Spring Boot & Keycloak";
-    }
-
-    @GetMapping("/admin")
-    @PreAuthorize("hasRole('client_admin')")
-    public String helloAdmin() {
-        return "Hello Admin from Spring Boot & Keycloak";
+        return "Hello, user!";
     }
 }
